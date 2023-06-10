d0236f883b57cd5574f21f12f80e63211edc3572
Salome Rao <salomrao.daliya@gmail.com>
2023-06-11 00:59:16 +0530

    added login page

--- Backend/Routes/CreateUser.js
+++ Backend/Routes/CreateUser.js
@@ -27,4 +27,26 @@ body("name").isLength({min:5})
     res.json({success:false});
    }
 })
+
+router.post("/loginuser",
+body("email").isEmail(),
+body("password",'Incorrect Password').isLength({min:5})
+,async(req,res)=>{
+ 
+  let email= req.body.email;
+   try{
+  let userdata= await User.findOne({email});
+    if(!userdata){
+      return res.status(400).json({errors:"Try logging with correct credentials"})
+    }
+    if(req.body.password!==userdata.password){
+      return res.status(400).json({errors:"Try logging with correct credentials"})
+   
+    }
+    return res.json({success:true})
+   }catch(error){
+    console.log(error)
+    res.json({success:false});
+   }
+})
 module.exports= router;
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,10 +1,54 @@
 import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login=()=>{
 
+        const[credential,setcredential]=useState({email:"",password:""})
+   
+     const handleSubmit= async(e)=>{
+       e.preventDefault();
+       const response = await fetch("http://localhost:5000/api/loginuser",{
+           method:'POST',
+           headers:{
+               'Content-Type':'application/json'
+           },
+           body:JSON.stringify({email:credential.email,password:credential.password})
+       });
+       const json = await response.json()
+       console.log(json);
+   
+       if(!json.success){
+   alert("enter valid credentials");
+       }else
+       alert("success");
+     }
+    const onchange=(event)=>{
+       setcredential({...credential,[event.target.name]:event.target.value})
+    }
 return(
     <div>
-    Login
+    
+    <div className='container m-3'>
+
+<form onSubmit={handleSubmit}>
+   
+
+<div class="mb-3">
+<label htmlFor="exampleInputEmail1" class="form-label">Email address</label>
+<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='email' value={credential.email} onChange={onchange}/>
+<div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+</div>
+<div class="mb-3">
+<label htmlFor="exampleInputPassword1" class="form-label">Password</label>
+<input type="password" class="form-control" id="exampleInputPassword1" name='password' value={credential.password} onChange={onchange}/>
+</div>
+
+
+<button type="submit" class="m-3 btn btn-success">Submit</button>
+<Link to="/createuser" className="m-3 btn btn-danger">I am a new User</Link>
+</form>
+</div>
     </div>
 )
 }
