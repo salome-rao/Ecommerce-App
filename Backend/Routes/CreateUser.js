b86aa3dc92a7e4da571caeccc9cbc81a9897e1b4
Salome Rao <76882669+salome-rao@users.noreply.github.com>
2023-09-25 03:28:51 +0530

    Update CreateUser.js

--- Backend/Routes/CreateUser.js
+++ Backend/Routes/CreateUser.js
@@ -3,7 +3,8 @@ const router = express.Router()
 const User = require("../models/User")
 const {body , validationResult}= require('express-validator');
 const bcrypt = require("bcryptjs");
-
+var jwt = require('jsonwebtoken');
+const jwtSecret = "HaHa"
 router.post("/creatuser",
 body("email").isEmail(),
 body("password",'Incorrect Password').isLength({min:5}),
@@ -34,6 +35,42 @@ body("name").isLength({min:5})
 })
  
 
+// router.post('/login', [
+//     body('email', "Enter a Valid Email").isEmail(),
+//     body('password', "Password cannot be blank").exists(),
+// ], async (req, res) => {
+//     let success = false
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//         return res.status(400).json({ errors: errors.array() })
+//     }
+
+//     const { email, password } = req.body;
+//     try {
+//         let user = await User.findOne({ email });  //{email:email} === {email}
+//         if (!user) {
+//             return res.status(400).json({ success, error: "Try Logging in with correct credentials" });
+//         }
+
+//         const pwdCompare = await bcrypt.compare(password, user.password); // this return true false.
+//         if (!pwdCompare) {
+//             return res.status(400).json({ success, error: "Try Logging in with correct credentials" });
+//         }
+//         const data = {
+//             user: {
+//                 id: user.id
+//             }
+//         }
+//         success = true;
+//         const authToken = jwt.sign(data, jwtSecret);
+//         res.json({ success, authToken })
+
+
+//     } catch (error) {
+//         console.error(error.message)
+//         res.send("Server Error")
+//     }
+// })
 
 
 router.post("/loginuser",
